ddf296ae17ac07ecd2c52086cedd460612a44dd8
Bera Yilmazyurt <bera.yilmazyurt@kartaca.com>
2024-05-09 14:20:36 +0300

    postgres connection

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"log"
+)
 
 func main() {
-	server := NewApıServer(":3005")
+	store, err := NewPostgresStore()
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := store.Init(); err != nil {
+		log.Fatal(err)
+	}
+	server := NewApıServer(":3005", store)
 	server.Run()
-	fmt.Println("Hello")
 }
